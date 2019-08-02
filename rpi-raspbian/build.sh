44a460d8fc642e1ebdbe3e1e068741ab69676550
Sven Berger <sven@berger-lembeck.de>
2019-08-02 20:11:03 +0200

    RPI Pilight/Homegear/Knxd/Base Images entfernt/verschoben

--- rpi-raspbian/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -t derbrummbaer/rpi-raspbian:jessie  ./jessie
-docker build -t derbrummbaer/rpi-raspbian:stretch ./stretch
-docker build -t derbrummbaer/rpi-raspbian:buster  ./buster
